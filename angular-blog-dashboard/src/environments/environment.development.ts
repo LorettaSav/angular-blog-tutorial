52eba30e776556d5af965c847737ec0ceb93676b
loretta savvidou <loretta.sav.6@gmail.com>
2023-08-04 03:13:55 +0200

    adding angular/fire and resolving issues

---
+++ angular-blog-dashboard/src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-blog-app-56197',
+    appId: '1:336388883178:web:7552726438d6615ccf4d93',
+    databaseURL: 'https://angular-blog-app-56197-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'angular-blog-app-56197.appspot.com',
+    apiKey: 'AIzaSyD_bltpbth6dvz_mnvRIKO_beCAdWWazMw',
+    authDomain: 'angular-blog-app-56197.firebaseapp.com',
+    messagingSenderId: '336388883178',
+  },};
---
+++ angular-blog-dashboard/src/environments/environment.ts
@@ -0,0 +1,20 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-blog-app-56197',
+    appId: '1:336388883178:web:7552726438d6615ccf4d93',
+    databaseURL: 'https://angular-blog-app-56197-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'angular-blog-app-56197.appspot.com',
+    apiKey: 'AIzaSyD_bltpbth6dvz_mnvRIKO_beCAdWWazMw',
+    authDomain: 'angular-blog-app-56197.firebaseapp.com',
+    messagingSenderId: '336388883178',
+  },
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyD_bltpbth6dvz_mnvRIKO_beCAdWWazMw',
+    authDomain: 'angular-blog-app-56197.firebaseapp.com',
+    projectId: 'angular-blog-app-56197',
+    storageBucket: 'angular-blog-app-56197.appspot.com',
+    messagingSenderId: '336388883178',
+    appId: '1:336388883178:web:7552726438d6615ccf4d93',
+  },
+};
